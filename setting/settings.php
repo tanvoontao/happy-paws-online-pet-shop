1205c9f2e5f855f4f4034513c65a2dd2a0923229
tanvoontao <tanvoontao0908@gmail.com>
2021-07-30 12:30:01 +0800

    Update settings.php

--- setting/settings.php
+++ setting/settings.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "happy_paws_online_dashboard_db";
+    $servername = "us-cdbr-east-04.cleardb.com";
+    $username = "b5987c9f3a39ce";
+    $password = "d1f6cdf5";
+    $dbname = "heroku_4ef650faeeb566c";
 
     // host name, database username, password, and database name.
     $conn = mysqli_connect($servername, $username, $password, $dbname);
